ece845dcce032f34ac12133351a6eeb6f2c8293a
dvaltrick <dvaltrick@gmail.com>
2017-06-15 14:48:54 -0300

    Start
    Teste

---
+++ XPesagem/XPesagem/Model/DataPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XPesagem.Model
+{
+    public class DataPoint
+    {
+        public string dataMarcacao;
+        public float peso;
+        public float meta;
+    }
+}
